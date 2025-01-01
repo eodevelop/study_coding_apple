918025330eca692465e74051963f7edc5c203095
eodevelop <eodevelop@naver.com>
2025-01-02 07:51:36 +0900

    임시 커밋

--- vue/code/vue-project/src/App.vue
+++ vue/code/vue-project/src/App.vue
@@ -1,20 +1,30 @@
 <template>
+  <div class="black-bg">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="item in menu" :key="item">{{ item }}</a>
   </div>
   <div>
+    <img src="./assets/img/room0.jpg" class="room-img" alt="원룸 사진 0">
     <h4>{{ products[0] }}</h4>
     <p>50만원</p>
     <button @click="increase(0)"> 허위 매물 신고</button>
     <span> 신고 수 : {{ reportCounts[0] }} </span>
   </div>
   <div>
+    <img src="./assets/img/room1.jpg" class="room-img" alt="원룸 사진 1">
     <h4>{{ products[1] }}</h4>
     <p>50만원</p>
     <button @click="increase(1)"> 허위 매물 신고</button>
     <span> 신고 수 : {{ reportCounts[1] }} </span>
   </div>
   <div>
+    <img src="./assets/img/room2.jpg" class="room-img" alt="원룸 사진 2">
     <h4>{{ products[2] }}</h4>
     <p>50만원</p>
     <button @click="increase(2)"> 허위 매물 신고</button>
@@ -30,6 +40,7 @@ export default {
       menu: ['Home', 'Products', 'About'],
       products: ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
       reportCounts: [0, 0, 0],
+      isModalOpen: false,
     };
   },
   methods: {
@@ -42,6 +53,29 @@ export default {
 </script>
 
 <style scoped>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 div {
   text-align: center;
 }
@@ -55,5 +89,10 @@ div {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
