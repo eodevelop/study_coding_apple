d0d80730608af16f48af47015ab63b64f4d87d95
eodevelop <eodevelop@naver.com>
2025-02-22 00:29:22 +0900

    숙제 완료

--- react/code/blog/src/App.jsx
+++ react/code/blog/src/App.jsx
@@ -3,7 +3,11 @@ import "./App.css";
 
 function App() {
   let post = "강남 우동 맛집";
-  let [글제목, set글제목] = useState("남자 코트 추천");
+  let [글제목, set글제목] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "파이썬 독학",
+  ]);
 
   return (
     <div className="App">
@@ -11,7 +15,15 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{글제목}</h4>
+        <h4>{글제목[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
