b0691f8604103bb917efec75c3393a837267ddf8
eodevelop <eodevelop@naver.com>
2025-02-22 00:24:11 +0900

    중요한 데이터는 변수말고 state에 담습니다 강의 완료

--- react/code/blog/src/App.jsx
+++ react/code/blog/src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   let post = "강남 우동 맛집";
+  let [글제목, set글제목] = useState("남자 코트 추천");
+
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{글제목}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
