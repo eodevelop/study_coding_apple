e2137a51a84e950ae33c76ee75b2077454b565ac
eodevelop <eodevelop@naver.com>
2025-03-03 15:21:31 +0900

    플러터 기본 앱 레이아웃 만드는 법 (Scaffold)
    숙제 완료

--- flutter/code/contact/lib/main.dart
+++ flutter/code/contact/lib/main.dart
@@ -10,15 +10,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue, // 앱 전체의 AppBar 배경색을 보라색으로 설정
+        ),
+      ),
       home: Scaffold(
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.star),
-            Icon(Icons.star),
-            Icon(Icons.star),
-          ],
-        )
+        appBar: AppBar(
+          title: Text("안녕"),
+        ),
+        body: Container(),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_page)
+            ],
+          ),
+        ),
       )
     );
   }
