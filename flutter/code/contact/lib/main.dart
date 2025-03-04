c2fc441018034e42473344d939f108b52e2c8fd8
eodevelop <eodevelop@naver.com>
2025-03-04 16:36:01 +0900

    플러터 Typography & 알아서 레이아웃 잘짜려면
    숙제 완료

--- flutter/code/contact/lib/main.dart
+++ flutter/code/contact/lib/main.dart
@@ -20,7 +20,35 @@ class MyApp extends StatelessWidget {
           title: Text("앱임"),
         ),
         body: SizedBox(
-          child: ElevatedButton(onPressed: () {}, child: Text('test')),
+          height: 150,
+          width: 500,
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/camera.png',
+                width: 150,
+                height: 150,
+              ),
+              Expanded(  // Column을 Expanded로 감싸기
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("캐논 DSLR 100D (단렌즈, 충전기 16기가 SD 포함)"),
+                    Text("성동구 행당동 끌올 10분전"),
+                    Text("210,000원"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(Icons.favorite_border),
+                        Text("2"),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
         ),
       )
     );
